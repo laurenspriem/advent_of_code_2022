3f5de8d3dcdc157c03a5c74597a2afb49181cee6
laurenspriem <github_uni.fishers@aleeas.com>
2022-12-06 08:19:43 +0100

    Day 6 complete

---
+++ Day 6 - Tuning Trouble/day6.py
@@ -0,0 +1,26 @@
+# load data
+with open("Day 6 - Tuning Trouble/input_sample.txt") as f:
+    challenge_input_sample = f.read()
+with open("Day 6 - Tuning Trouble/input.txt") as f:
+    challenge_input = f.read()
+
+
+def part1(challenge_input):
+    characters_list = []
+    for count, character in enumerate(challenge_input):
+        characters_list.append(character)
+        if count >=3:
+            if len(set(characters_list[-4:])) == 4:
+                return count+1
+
+print(f"Answer: {part1(challenge_input)}")
+
+def part2(challenge_input):
+    characters_list = []
+    for count, character in enumerate(challenge_input):
+        characters_list.append(character)
+        if count >=13:
+            if len(set(characters_list[-14:])) == 14:
+                return count+1
+
+print(f"Answer: {part2(challenge_input)}")
