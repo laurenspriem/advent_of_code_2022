f0b7e9b59092c99a6d5afa9f4a40133e1f0ccc8c
laurenspriem <github_uni.fishers@aleeas.com>
2023-01-07 12:58:32 +0100

    Day 12 improved

--- python/day12.py
+++ python/day12.py
@@ -6,7 +6,6 @@ with open("../input/day12_input.txt", encoding="utf-8") as f:
 
 from heapq import heapify, heappop
 from math import inf
-from tqdm import tqdm
 
 
 def relax(u, v, Q, shortest, pred):
@@ -67,49 +66,44 @@ print(f"Answer: {part1(challenge_input)}")
 
 def part2(challenge_input_variable):
     alphabet_height = "abcdefghijklmnopqrstuvwxyz"
-    all_a = [
-        (y, x)
-        for y, row in enumerate(challenge_input_variable.splitlines())
-        for x, alpha in enumerate(row)
-        if alpha == "a"
-    ]
-    all_shortest = {}
-    for a in tqdm(all_a):
-        Q, shortest, pred, height = [], {}, {}, {}
-        for y, row in enumerate(challenge_input_variable.splitlines()):
-            for x, alpha in enumerate(row):
-                pred[(y, x)] = None
-                if (y, x) == a:
-                    shortest[(y, x)] = 0
-                    Q.append([0, (y, x)])
-                    height[(y, x)] = 0
-                elif alpha == "E":
-                    E = (y, x)
-                    shortest[(y, x)] = inf
-                    Q.append([inf, (y, x)])
-                    height[(y, x)] = 25
-                else:
-                    shortest[(y, x)] = inf
-                    Q.append([inf, (y, x)])
-                    height[(y, x)] = alphabet_height.find(alpha)
-        heapify(Q)
-        while Q:
-            u = heappop(Q)
-            adjacents = [
-                (u[1][0] + 1, u[1][1]),
-                (u[1][0] - 1, u[1][1]),
-                (u[1][0], u[1][1] + 1),
-                (u[1][0], u[1][1] - 1),
-            ]
-            for v in adjacents:
-                try:
-                    if height[u[1]] + 1 >= height[v]:
-                        Q, shortest, pred = relax(u, v, Q, shortest, pred)
-                except KeyError as ke:
-                    continue
-        all_shortest[a] = shortest[E]
+    Q, shortest, pred, height = [], {}, {}, {}
+    for y, row in enumerate(challenge_input_variable.splitlines()):
+        for x, alpha in enumerate(row):
+            pred[(y, x)] = None
+            if alpha == "S":
+                S = (y, x)
+                shortest[(y, x)] = inf
+                Q.append([inf, (y, x)])
+                height[(y, x)] = 0
+            elif alpha == "E":
+                E = (y, x)
+                shortest[(y, x)] = 0
+                Q.append([0, (y, x)])
+                height[(y, x)] = 25
+            else:
+                shortest[(y, x)] = inf
+                Q.append([inf, (y, x)])
+                height[(y, x)] = alphabet_height.find(alpha)
+    heapify(Q)
+    while Q:
+        u = heappop(Q)
+        adjacents = [
+            (u[1][0] + 1, u[1][1]),
+            (u[1][0] - 1, u[1][1]),
+            (u[1][0], u[1][1] + 1),
+            (u[1][0], u[1][1] - 1),
+        ]
+        for v in adjacents:
+            try:
+                if height[u[1]] <= height[v] + 1:
+                    Q, shortest, pred = relax(u, v, Q, shortest, pred)
+            except KeyError as ke:
+                continue
+        if height[u[1]] == 0:
+            answer = shortest[u[1]]
+            break
 
-    return min(all_shortest.values())
+    return answer  # shortest[E]
 
 
 print(f"Answer: {part2(challenge_input)}")
