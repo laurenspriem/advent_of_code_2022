bcbb583b6b3dec615ca9d91cc3b258e817c9050e
laurenspriem <github_uni.fishers@aleeas.com>
2023-01-07 13:06:56 +0100

    Day 13 initialize

---
+++ python/day13.py
@@ -0,0 +1,11 @@
+# load data
+with open("../input/day13_input_sample.txt", encoding="utf-8") as f:
+    challenge_input_sample = f.read()
+with open("../input/day13_input.txt", encoding="utf-8") as f:
+    challenge_input = f.read()
+
+
+def part1(challenge_input):
+    pairs = challenge_input.split("\n\n")
+
+    pass
