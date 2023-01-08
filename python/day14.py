d535eac1c3b5826291e03f5fa213d03ee3ed2cbb
laurenspriem <github_uni.fishers@aleeas.com>
2023-01-08 21:48:28 +0100

    Day 14 initialization

---
+++ python/day14.py
@@ -0,0 +1,13 @@
+# load data
+with open("input/day14_input_sample.txt", encoding="utf-8") as f:
+    challenge_input_sample = f.read()
+with open("input/day14_input.txt", encoding="utf-8") as f:
+    challenge_input = f.read()
+
+
+def part1(challenge_input):
+
+    return 0
+
+
+part1(challenge_input_sample)
